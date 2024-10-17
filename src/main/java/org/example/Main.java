c075824e35648c275ddd72d845defbcfbf3999c0
Ronald Espitia <ronald79@gmail.com>
2024-10-16 22:12:12 -0500

    Clase Oct 15

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola Juan David");
+        // github Juan David: PilotK300
+
+        // Tipos de datos Primitivos
+        int numero = 10; // rango -2,147,483,648 hasta 2,147,483,647
+
+        double temperatura = 18.5; // Doble precision aprox. 15-16 digitos decimales
+
+        float altura = 1.99f; // precision simple aprox 7 digitos decimales
+
+        char letra = 'A';
+
+        boolean aceptado = true;
+
+        byte numeroByte = 120; //rango -128 a 127
+
+        short numeroShort = 20000; // -32768 a 32767
+
+        long numeroLargo = 46464654846L; //-9,223,372,036,854,775,808 hasta 9,223,372,036,854,775,807
+
+        numero = 20;
+
+    // CONSTANTES
+
+        final double NUMERO_PI = 3.1416;
+
+    // Imprimir info en consola
+
+        System.out.print(numeroLargo);
+        System.out.println(numeroShort);
+        System.out.print(numeroByte);
+
+    }
+}
