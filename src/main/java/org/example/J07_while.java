28a02878ad9784e80d58050f0d847505cbe53706
Ronald Espitia <ronald79@gmail.com>
2024-10-28 14:49:02 -0500

    Clase

---
+++ src/main/java/org/example/J07_while.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class J07_while {
+    // while
+//        int numero = 0;
+//        while(numero < 5){
+//            System.out.println(numero);
+//            numero++;
+//        }
+
+    // while con break
+//        int numero = 0;
+//        while(numero < 10){
+//            if (numero == 5){
+//                break;
+//            }
+//            System.out.println(numero);
+//            numero++;
+//        }
+
+    // while con continue
+    int numero = 0;
+
+//        while (numero < 9) {
+//        numero++;
+//        if (numero == 5) {
+//            continue;
+//        }
+//        System.out.println(numero);
+//    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,33 +6,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // while
-//        int numero = 0;
-//        while(numero < 5){
-//            System.out.println(numero);
-//            numero++;
-//        }
 
-        // while con break
-//        int numero = 0;
-//        while(numero < 10){
-//            if (numero == 5){
-//                break;
-//            }
-//            System.out.println(numero);
-//            numero++;
-//        }
-
-        // while con continue
-        int numero = 0;
-
-        while (numero < 9) {
-            numero++;
-            if (numero == 5) {
-                continue;
-            }
-            System.out.println(numero);
-        }
     }
 }
 
