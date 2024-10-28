7edc1de06dc765987cb83c39482e576a2c47f952
Ronald Espitia <ronald79@gmail.com>
2024-10-28 17:18:43 -0500

    Clase oct 28

---
+++ src/main/java/org/example/J08_for.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class J08_for {
+            /* for (var. inicializacion; condicion; actualizacion){
+         bloque de codigo a repetir}
+         */
+
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println("valor de i: " + i);
+ //   }
+
+//        for (int j = 0; j < 10; j++){
+//        if (j == 8){
+//            break;
+//        }
+//        if (j % 2 != 0){
+//            continue;
+//        }
+////            System.out.println(j);
+//    }
+
+    String texto = "Hola";
+//        System.out.println(texto.charAt(5));
+
+/*        for (int x = 0; x < texto.length(); x++){
+        System.out.println("Caracter en el indice " + x + " : " + texto.charAt(x));
+    }*/
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,31 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int resultado = multiplicar(2,2,2);
+        System.out.println(resultado);
+    }
 
+    // Metodo Void sin parametros
+    public static void metodoVoidSinParams() {
+        System.out.println("Soy un metodo void sin parametros");
+    }
+
+    // Metodo void con parametros
+    public static void sumar(int numero1, int otroNumero) {
+        System.out.println(numero1 + otroNumero);
+    }
+
+    // Metodo con retorno y sin parametros
+    public static int sumarR(){
+        int a = 5;
+        int b = 10;
+        return a + b;
+    }
+
+    // Metodo con retorno y con parametros
+    public static int multiplicar(int n1, int n2, int n3){
+        int multi = n1 * n2 * n3;
+        return multi;
     }
 }
 
